98b8288fa86f248e40248207b4241cdfc67e024b
Haoyuan Zeng <haoyuan.zeng@econ.uzh.ch>
2021-10-24 15:39:11 +0200

    clean data and prepare for analysis

--- clean_data.R
+++ clean_data.R
@@ -3,41 +3,77 @@ rm(list=ls())
 # clean each dataset ----------------------------------------------------------
 # read covid dataset
 library(readxl)
-covid <- read_xlsx("raw_data/covid_data.xlsx" )
+covid <- read_xlsx("data/covid_data.xlsx")
+
+# remove some non-country observations and adjust country codes for merging
+covid <- covid[is.na(covid$continent) == FALSE,]
+covid[covid$iso_code=="OWID_KOS", "iso_code"] <- 'XKX'
+
 # keep useful var only
 library(dplyr)
-covid2 <- subset(covid, select = c("iso_code", "continent", "location", "total_cases",
-                                   "total_deaths","total_cases_per_million","total_deaths_per_million",
-                                   "positive_rate","people_fully_vaccinated","people_fully_vaccinated_per_hundred",
-                                   "population","population_density","median_age","aged_65_older","gdp_per_capita","human_development_index"))
+covid2 <- subset(covid, select = 
+                   c("iso_code", "continent", "location", "total_cases",
+                     "total_deaths","total_cases_per_million","total_deaths_per_million",
+                     "positive_rate","people_fully_vaccinated","people_fully_vaccinated_per_hundred",
+                     "population","population_density","median_age","aged_65_older","gdp_per_capita",
+                     "human_development_index"))
 
 
 # read culture data
 library(readr)
-culture = read_delim("raw_data/culture_data.csv", delim = ";", col_names = TRUE)
+culture = read_delim("data/culture_data.csv", delim = ";", col_names = TRUE)
 # only keep idv: individualism vs. collectivism
 culture2 <- subset(culture, select = c("ctr", "country","idv"))
 # rename
 culture2 <- rename(culture2, iso_code = ctr)
 # change idv to numeric var
 culture2$idv <- as.numeric(culture2$idv)
+# remove Null data
+culture2 <- culture2[is.na(culture2$idv) == FALSE,]
+# adjust country name for merging
+culture2[culture2$iso_code == 'CZE','country'] <- 'Czechia'
+culture2[culture2$iso_code == 'GBR','country'] <- 'United Kingdom'
+culture2[culture2$iso_code == 'KOR','country'] <- 'South Korea'
+culture2[culture2$iso_code == 'SLK','country'] <- 'Slovakia'
+culture2[culture2$iso_code == 'USA','country'] <- 'United States'
 
 # read democracy data
 library(readxl)
-demo <- read_xls("raw_data/polity5.xls" )
+demo <- read_xls("data/polity5.xls" )
 # only keep most recent 5 years: 2014-2018
 demo2 <- filter(demo, year>=2014 & year<=2018)
-# only useful var
+# keep only useful var
 demo2 <- subset(demo2, select = c("scode", "country", "year", "democ","polity2"))
 # democ is a measure for democracy, policy2 is a measure for a combination of democracy and autocracy
 # replace democ = NA if it's negative (which means transition or missing)
 demo2$democ[demo2$democ<0] <- NA
 # calculate five-year average
-demo_sum <- demo2 %>% group_by(country, scode) %>% summarize(mean(democ,na.rm = TRUE), mean(polity2,na.rm = TRUE))
-demo_sum <- rename(demo_sum, democ = "mean(democ, na.rm = TRUE)", polity2 = "mean(polity2, na.rm = TRUE)", iso_code = scode)
+demo_sum <- demo2 %>% 
+  group_by(country, scode) %>% 
+  summarize(mean(democ,na.rm = TRUE), mean(polity2,na.rm = TRUE))
+demo_sum <- rename(demo_sum, democ = "mean(democ, na.rm = TRUE)", 
+                   polity2 = "mean(polity2, na.rm = TRUE)", iso_code = scode)
+
+# adjust country name for merging
+demo_sum[demo_sum$country == 'Cote D\'Ivoire','country'] <- 'Cote d\'Ivoire'
+demo_sum[demo_sum$iso_code == 'CZR','country'] <- 'Czechia'
+demo_sum[demo_sum$iso_code == 'CZR','country'] <- 'Czechia'
+demo_sum[demo_sum$iso_code == 'ROK','country'] <- 'South Korea'
+demo_sum[demo_sum$iso_code == 'MYA','country'] <- 'Myanmar'
+demo_sum[demo_sum$iso_code == 'SLO','country'] <- 'Slovakia'
+demo_sum[demo_sum$iso_code == 'CON','country'] <- 'Congo'
+demo_sum[demo_sum$iso_code == 'ZAI','country'] <- 'Democratic Republic of Congo'
+demo_sum[demo_sum$iso_code == 'SDN','country'] <- 'Sudan'
+demo_sum[demo_sum$iso_code == 'SWA','country'] <- 'Eswatini'
+demo_sum[demo_sum$iso_code == 'SWA','country'] <- 'Eswatini'
+demo_sum[demo_sum$iso_code == 'ETM','country'] <- 'Timor'
+demo_sum[demo_sum$iso_code == 'ETM','country'] <- 'Timor'
+demo_sum[demo_sum$iso_code == 'UAE','country'] <- 'United Arab Emirates'
+demo_sum[demo_sum$iso_code == 'MAC','country'] <-'North Macedonia'
+
 
 # read state capacity data
-capacity = read_delim("raw_data/state_capacity.tab", col_names = TRUE)
+capacity = read_delim("data/state_capacity.tab", col_names = TRUE)
 # only keep most recent 5 years: 2014-2018
 capacity2 <- filter(capacity, year>=2014 & year<=2018)
 # only useful var
@@ -47,21 +83,26 @@ capacity2 <- subset(capacity2, select = c("ISO3", "country", "year", "ape1","rpe
 # rprwork: RPR gauges the capacity of governments to mobilize populations under their control.
 # rpafull: Relative Political Allocation is a composite indicator to measure how public expenditures are prioritized in the government budget, RPA measures the gaps between the actual percentage distribution of general government outlays and what the estimated percentages of the “best” expenditure portfolio to allocate to each functional area to maximize the living standards of the population. 
 # calculate five-year average
-capacity_sum <- capacity2 %>% group_by(country, ISO3) %>% summarize(mean(ape1,na.rm = TRUE), mean(rpegdp,na.rm = TRUE), mean(rprwork,na.rm = TRUE), mean(rpafull,na.rm = TRUE))
-capacity_sum <- rename(capacity_sum, ape = "mean(ape1, na.rm = TRUE)", rpe = "mean(rpegdp, na.rm = TRUE)", rpr = "mean(rprwork, na.rm = TRUE)", rpa = "mean(rpafull, na.rm = TRUE)", iso_code = ISO3)
+capacity_sum <- capacity2 %>% 
+  group_by(country, ISO3) %>% 
+  summarize(mean(ape1,na.rm = TRUE), 
+            mean(rpegdp,na.rm = TRUE), 
+            mean(rprwork,na.rm = TRUE), 
+            mean(rpafull,na.rm = TRUE))
+capacity_sum <- rename(capacity_sum, 
+                       ape = "mean(ape1, na.rm = TRUE)", 
+                       rpe = "mean(rpegdp, na.rm = TRUE)", 
+                       rpr = "mean(rprwork, na.rm = TRUE)", 
+                       rpa = "mean(rpafull, na.rm = TRUE)", 
+                       iso_code = ISO3)
 
 
-# merge datasets ----------------------------------------------------------
-# merge covid with democracy data
-merge_1 <- full_join(covid2, demo_sum, by = "iso_code")
-View(dplyr::select(merge_1,c('iso_code','location', 'country','democ','polity2')))
+# merge data ----------------------------------------------------------
 
-# merge covid with culture data
-merge_2 <- full_join(covid2, culture2, by = "iso_code")
-View(dplyr::select(merge_2,c('iso_code','location', 'country','idv')))
+covid_analysis <- covid2 %>% 
+  left_join(demo_sum, by = c("location" = "country")) %>% 
+  left_join(culture2, by = c("location" = "country")) %>% 
+  left_join(capacity_sum, by = "iso_code")
 
-# merge covid with capacity data
-merge_3 <- full_join(covid2, capacity_sum, by = "iso_code")
-View(dplyr::select(merge_3,c('iso_code','location', 'country','ape','rpe','rpr','rpa')))
-
+write.csv(covid_analysis, 'data/covid_analysis.csv')
 
--- download_data.R
+++ download_data.R
@@ -2,60 +2,31 @@ rm(list=ls())
 
 # Create a folder to store raw data ---------------------------------------
 
-if (!dir.exists('raw_data')){
-  dir.create('raw_data')
+if (!dir.exists('data')){
+  dir.create('data')
 }
-
-# Download country code data 1----------------------------------------------
-
-library("jsonlite")
-
-json_file <- 'https://datahub.io/JohnSnowLabs/country-and-continent-codes-list/datapackage.json'
-json_data <- fromJSON(paste(readLines(json_file), collapse=""))
-
-for(i in 1:length(json_data$resources$datahub$type)){
-  if(json_data$resources$datahub$type[i]=='derived/csv'){
-    path_to_file = json_data$resources$path[i]
-    download.file(url = path_to_file, destfile = 'raw_data/country_code_list_1.csv')
-  }
-}
-
-# Download country code data 2 --------------------------------------------
-
-json_file <- 'https://datahub.io/core/country-codes/datapackage.json'
-json_data <- fromJSON(paste(readLines(json_file), collapse=""))
-
-
-for(i in 1:length(json_data$resources$datahub$type)){
-  if(json_data$resources$datahub$type[i]=='derived/csv'){
-    path_to_file = json_data$resources$path[i]
-    data <- read.csv(url(path_to_file))
-    download.file(url = path_to_file, destfile = 'raw_data/country_code_list_2.csv')
-  }
-}
-
 
 # Download covid data from our world in data (with country code) --------------------------------------------
 download.file(url = 'https://github.com/owid/covid-19-data/raw/master/public/data/latest/owid-covid-latest.xlsx',
-              destfile = 'raw_data/covid_data.xlsx')
+              destfile = 'data/covid_data.xlsx')
 
 # Download culture data ---------------------------------------------------
 
 download.file(url = 'http://geerthofstede.com/wp-content/uploads/2016/08/6-dimensions-for-website-2015-08-16.csv',
-              destfile = 'raw_data/culture_data.csv')
+              destfile = 'data/culture_data.csv')
 
 
-# Download institutional data ---------------------------------------------
+# Download democracy data ---------------------------------------------
 
-# demovracy: use polity 5 data
+# use polity 5 data
 download.file(url = 'http://www.systemicpeace.org/inscr/p5v2018.xls',
-              destfile = 'raw_data/polity5.xls')
+              destfile = 'data/polity5.xls')
 
 
 # Download state capacity data --------------------------------------------
 
 download.file(url = 'https://dataverse.harvard.edu/api/access/datafile/:persistentId?persistentId=doi:10.7910/DVN/NRR7MB/6DC2YR',
-              destfile = 'raw_data/state_capacity.tab')
+              destfile = 'data/state_capacity.tab')
 
 
 
